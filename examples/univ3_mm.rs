20d183e3ab46e08ebc489b2f37dac2d6803aa6c9
10xwannabe <moverxyz@gmail.com>
2023-09-21 03:14:26 +0300

    naive 64.96->63.63 requantization

--- examples/univ3_mm.rs
+++ examples/univ3_mm.rs
@@ -1,75 +1,80 @@
 use clap::Parser;
-use halo2_base::gates::{GateChip, GateInstructions};
-use halo2_base::utils::ScalarField;
+use halo2_base::gates::GateInstructions;
+use halo2_base::safe_types::RangeInstructions;
+use halo2_base::utils::{BigPrimeField, ScalarField};
 use halo2_base::AssignedValue;
-#[allow(unused_imports)]
-use halo2_base::{
-    Context,
-    QuantumCell::{Constant, Existing, Witness},
-};
+use halo2_base::Context;
+use halo2_base::QuantumCell::Constant;
+use halo2_scaffold::gadget::fixed_point::{FixedPointChip, FixedPointInstructions};
 use halo2_scaffold::scaffold::cmd::Cli;
 use halo2_scaffold::scaffold::run;
+use num_bigint::BigUint;
+use num_traits::FromPrimitive;
 use serde::{Deserialize, Serialize};
+#[allow(unused_imports)]
+use std::env::{set_var, var};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct CircuitInput {
     pub x: String, // field element, but easier to deserialize as a string
 }
 
-// this algorithm takes a public input x, computes x^2 + 72, and outputs the result as public output
+// https://hackmd.io/@tazAymRSQCGXTUKkbh1BAg/r1vcQLl8is
+fn sqrtx96_to_63fp63<F: ScalarField>(
+    ctx: &mut Context<F>,
+    sqrtx96: &AssignedValue<F>,
+    fpc: FixedPointChip<F, 63>,
+) where
+    F: BigPrimeField,
+{
+    let range_chip = fpc.range_gate();
+
+    let (q, _) = range_chip.div_mod(ctx, *sqrtx96, 1_u128 << 32, 160);
+    // println!("x: {:?}", *sqrtx96.value());
+    // println!("q: {:?}", q.value());
+    // println!("r: {:?}", r.value());
+
+    let q_sq = range_chip.gate.mul(ctx, q, q);
+    // println!("qq: {qq:?}");
+
+    let bu = BigUint::from_u128(2).unwrap();
+    let d_2_128 = BigUint::pow(&bu, 128);
+    // println!("d_2_128: {:?}", d_2_128);
+    let (q2, _) = range_chip.div_mod(ctx, q_sq, d_2_128, 160);
+    let a = Constant(F::from_str_vartime("9223372036854775808").unwrap());
+    let q3 = range_chip.gate.mul(ctx, q2, a);
+
+    let ten2twelve = fpc.quantization(1000000000000.0);
+    let q4 = fpc.qdiv(ctx, q3, Constant(ten2twelve));
+    let d = fpc.dequantization(*q4.value());
+    println!("d: {d:?}");
+}
+
 fn some_algorithm_in_zk<F: ScalarField>(
     ctx: &mut Context<F>,
     input: CircuitInput,
     make_public: &mut Vec<AssignedValue<F>>,
-) {
+) where
+    F: BigPrimeField,
+{
     let x = F::from_str_vartime(&input.x).expect("deserialize field element should not fail");
-    // `Context` can roughly be thought of as a single-threaded execution trace of a program we want to ZK prove. We do some post-processing on `Context` to optimally divide the execution trace into multiple columns in a PLONKish arithmetization
-    // More advanced usage with multi-threaded witness generation is possible, but we do not explain it here
+    const PRECISION_BITS: u32 = 63;
+    let lookup_bits =
+        var("LOOKUP_BITS").unwrap_or_else(|_| panic!("LOOKUP_BITS not set")).parse().unwrap();
+    let fpc = FixedPointChip::<F, PRECISION_BITS>::default(lookup_bits);
 
-    // first we load a number `x` into as system, as a "witness"
+    // fixed-point exp arithmetic
     let x = ctx.load_witness(x);
-    // by default, all numbers in the system are private
-    // we can make it public like so:
     make_public.push(x);
-
-    // create a Gate chip that contains methods for basic arithmetic operations
-    let gate = GateChip::<F>::default();
-
-    // ===== way 1 =====
-    // now we can perform arithmetic operations almost like a normal program using halo2-lib API functions
-    // square x
-    let x_sq = gate.mul(ctx, x, x);
-
-    // x^2 + 72
-    let c = F::from(72);
-    // the implicit type of most variables is an "Existing" assigned value
-    // a known constant is a separate type that we specify by `Constant(c)`:
-    let out = gate.add(ctx, x_sq, Constant(c));
-    // Halo2 does not distinguish between public inputs vs outputs because the verifier seems them all at the same time
-    // However in traditional terms, `out` is our output number. It is currently still private.
-    // Let's make it public:
-    make_public.push(out);
-    // ==== way 2 =======
-    // here is a more optimal way to compute x^2 + 72 using the lower level `assign_region` API:
-    let val = *x.value() * x.value() + c;
-    let _val_assigned =
-        ctx.assign_region_last([Constant(c), Existing(x), Existing(x), Witness(val)], [0]);
-    // the `[0]` tells us to turn on a vertical `a + b * c = d` gate at row position 0.
-    // this imposes the constraint c + x * x = val
-
-    // ==== way 3 ======
-    // this does the exact same thing as way 2, but with a pre-existing function
-    let _val_assigned = gate.mul_add(ctx, x, x, Constant(c));
-
-    println!("x: {:?}", x.value());
-    println!("val_assigned: {:?}", out.value());
-    assert_eq!(*x.value() * x.value() + c, *out.value());
+    sqrtx96_to_63fp63(ctx, &x, fpc);
 }
 
 fn main() {
     env_logger::init();
-
-    let args = Cli::parse();
+    // genrally lookup_bits is degree - 1
+    // set_var("LOOKUP_BITS", 12.to_string());
+    // set_var("DEGREE", 13.to_string());
+    let args: Cli = Cli::parse();
 
     // run different zk commands based on the command line arguments
     run(some_algorithm_in_zk, args);
